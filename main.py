37f18959c4d6837803349a71081d126cdfa6430e
claudia-caeiro <claudiasduarte@aejcardosopires.pt>
2025-07-21 16:14:22 +0100

    libs

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 """
 Criaste o ficheiro que está no onedrive, documentos, python, projeto, projeto
 """
+
+From minhas_libs import ler_ficheiro
+    except IOError:
+        print(f"Erro: Não foi possível ler o ficheiro '{nome_ficheiro}'.")
+        return None
+"""
+def main():
+    nome_ficheiro = 'exemplo.txt'
+    conteudo = ler_ficheiro(nome_ficheiro)
+    if conteudo is not None:
+        print("Conteúdo do ficheiro:")
+        print(conteudo)
+"""
+Arquivo criado para armazenar funções que podem ser utilizadas em outros scripts.
+
+"""
---
+++ minhas_libs.py
@@ -0,0 +1,10 @@
+"""
+Arquivo criado para armazenar funções que podem ser utilizadas em outros scripts.""
+def ler_ficheiro(nome_ficheiro):
+    """
+    Função para ler o conteúdo de um ficheiro.
+    :param nome_ficheiro: Nome do ficheiro a ser lido .txt
+
+
+    :return: Conteúdo do ficheiro ou None se ocorrer um erro.
+    """
